0de20949f83063b68a58eff2e0083e0ad3bd5b56
Your Name <your.email@example.com>
2025-02-04 01:38:52 +0530

    photos change

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let btn = document.querySelector('button')
 
  let arrImage = [
-    "https://th.bing.com/th/id/OIP.0Tsi2T8-06Vxt0aWvo26RAHaMV?w=156&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-     "https://th.bing.com/th/id/OIP.q4XRuQWrO7hxDmA8gLRfAQHaFj?w=211&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-    "https://th.bing.com/th/id/OIP.vhId6NDTyDJiRiYTz-QqvQHaE8?w=261&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-    "https://th.bing.com/th/id/OIP.DyJmoOKCgaiTsvYK_VW0hAHaHB?w=172&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+    "https://th.bing.com/th/id/OIP.jhXnUwoGlpMUib5c39_sUwHaHa?w=165&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+     "https://dp-pic.com/wp-content/uploads/2024/03/a-cute-little-baby-dp-by-dp-pic-baby-dp-cute-dp-cute-pfp-2.jpg",
+    "https://th.bing.com/th/id/OIP.rfv7nHYjw6FgCVPhBF7yzwHaJP?pid=ImgDet&w=184&h=229&c=7&dpr=1.3",
+    "https://th.bing.com/th/id/OIP.UIi6bb0EYAk730qvvObcrgHaHa?pid=ImgDet&w=184&h=184&c=7&dpr=1.3"
  ]
  function randomImages (){
      let randomValue = Math.floor(Math.random()* arrImage.length)
